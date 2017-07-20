42523fb1a301e3d078394cb9043a208f2d3e586d
Nakamura <Nakamura@DESKTOP-ORNQG7R>
2017-07-21 01:36:13 +0600

    Добавьте файлы проекта.

---
+++ FolderInfo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FolderInfo
+{
+    class Program
+    {
+        public static StreamWriter strmW = new StreamWriter("FILES.txt");
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            DirectoryInfo dir = new DirectoryInfo(@"C:\");
+
+            PrintFiles(dir);
+            strmW.Close();
+            Console.WriteLine("Done");
+
+            Console.ReadKey();
+        }
+        static void PrintFiles(DirectoryInfo dir)
+        {
+            try
+            {
+                foreach (var file in dir.GetFileSystemInfos())
+                { strmW.WriteLine(file.FullName); }
+                foreach (var path in dir.GetDirectories()
+                    .Where(d => !d.Attributes.HasFlag(FileAttributes.NotContentIndexed)))
+                    PrintFiles(path);
+            }
+            catch(Exception ex) { }
+        }
+    }
+}
